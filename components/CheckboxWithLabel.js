73107c221768c21a49d286c3a4d5584453ea6389
jhta <jeisonhs93@gmail.com>
2015-09-21 16:52:49 -0500

    first commit

---
+++ components/CheckboxWithLabel.js
@@ -0,0 +1,15 @@
+var React = require('react/addons');
+var CheckboxWithLabel = React.createClass({
+  render() {
+    return (
+        <div>
+          <p>hola</p>
+          <ul>
+            <li>uno</li>
+            <li>dos</li>
+          </ul>
+        </div>
+    );
+  }
+});
+export default CheckboxWithLabel;
